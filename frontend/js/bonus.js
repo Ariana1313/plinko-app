d05cd1d7718d1112c4dc29b5ccdd8c23750c02b7
Ariana1313 <arianabuchanan914@gmail.com>
2025-12-09 12:24:38 +0100

    Delete frontend directory

--- frontend/js/bonus.js
+++
@@ -1,8 +0,0 @@
-async function addBonusToUser(userId, amount=150){
-  try{
-    const res = await fetch((API || '') + '/api/add-bonus', { method:'POST', headers:{'Content-Type':'application/json'}, body: JSON.stringify({ userId, amount }) });
-    const j = await res.json();
-    if(!res.ok) throw new Error(j.error || 'Add bonus failed');
-    return j.balance;
-  }catch(e){ throw e; }
-}
